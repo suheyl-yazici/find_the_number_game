49a4ab7c21ac6c818d7529537b01727507302f40
SüheyL <h.suheylyazici@gmail.com>
2022-02-12 19:28:22 +0300

    Algoritma kuruldu

--- apps.js
+++ apps.js
@@ -1,2 +1,38 @@
 let holdNumber = Math.floor(Math.random()*101);
 console.log(holdNumber);
+
+let takenNumber = document.getElementById("number");
+// console.log(takenNumber);
+
+let counter = 0 ;
+let magicHat = document.getElementById("hat");
+let resultNumber = document.getElementById("r_number");
+let attemptNumber = document.getElementById("attempt");
+let buttonCheck = document.getElementById("c_button"); 
+
+buttonCheck.addEventListener("click", () => {
+    while (true) {
+        if (holdNumber > Number(takenNumber.value)) {
+            alert(`Lütfen girdiğiniz sayı ${takenNumber.value} ile 100 arasında olsun`);
+            counter++;
+            break;
+        } else if (holdNumber < Number(takenNumber.value)){
+            alert(`Lütfen girdiğiniz sayı 0 ile ${takenNumber.value} arasında olsun`);
+            counter++;
+            break;
+        } else {
+            counter++;
+            img.style.display = "block";
+            (attemptNumber).innerHTML = "Number of attempts : " + counter;
+            (resultNumber).innerHTML = "Holden number is: " + holdNumber;
+            break;
+        }   
+    };
+
+});
+
+
+
+
+
+
